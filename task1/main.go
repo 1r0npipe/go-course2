079cc08e3175efd12d8c36c25992f507c52fe295
Eugene Makhinya <eugene.makhinya@gmail.com>
2021-02-21 16:07:10 +0000

    fix added by @Egor comments

--- task1/main.go
+++ task1/main.go
@@ -13,14 +13,13 @@ type myErrorType struct {
 }
 
 func (e *myErrorType) Error() string {
-	return "The error occurs: " + e.errorMessage + " with timestamp: " + e.time.Format("15:04:05.00000")
+	return fmt.Sprintf("The error occurs: %v with timestamp: %v",e.errorMessage ,e.time.Format("15:04:05.00000"))
 }
 
-func recovering() {
-	if err := recover(); err != nil {
-		log.Println("Panic occurs here: ", err)
-	}
-	fmt.Println("Time stamp of panic: " + time.Now().Format("15:04:05.00000"))
+func main() {
+	panicOutOfRange()
+	panicManually(true)
+	readCreateFile()
 }
 
 func panicOutOfRange() {
@@ -31,11 +30,9 @@ func panicOutOfRange() {
 	}
 }
 
-func recoveringWithTime() {
-	if smth := recover(); smth != nil {
-		if err, ok := smth.(myErrorType); ok {
-			fmt.Println(err.Error())
-		}
+func recovering() {
+	if err := recover(); err != nil {
+		log.Println("Panic occurs here: ", err)
 	}
 }
 
@@ -49,17 +46,22 @@ func panicManually(flag bool) {
 	}
 }
 
-func main() {
-	fmt.Println("=== Start of program ===")
-	panicOutOfRange()
-	fmt.Println("===  ===")
-	panicManually(true)
-	fmt.Println("=== End of program ===")
+func recoveringWithTime() {
+	if smth := recover(); smth != nil {
+		if err, ok := smth.(myErrorType); ok {
+			fmt.Printf("The error \"%s\" occurs at: %v\n", err.errorMessage, err.time.Format("15:04:05.00000"))
+		}
+	}
+}
+
+func readCreateFile() error {
 	file, err := os.OpenFile("test.txt", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Printf("File cannot be created, because of %v", err)
+		return err
 	}
 
 	defer file.Close()
 	_, _ = fmt.Fprintln(file, "Test data")
+	return nil
 }
