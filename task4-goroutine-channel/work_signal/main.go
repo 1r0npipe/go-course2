c185a3c26e04d8b3dab5c168bcf8a38f1464e74d
Eugene Makhinya <eugene.makhinya@gmail.com>
2021-03-04 16:47:19 +0000

    update

--- task4-goroutine-channel/work_signal/main.go
+++ task4-goroutine-channel/work_signal/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,14 +14,21 @@ func main() {
 	tasks := make(chan struct{})
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	go func() {
+		signal := <-signals
+		log.Printf("system call: %+v", signal)
+		cancelFunc()
+	}()
 	fmt.Println("PID-", os.Getpid())
+	ctxShutDown, cancelServ := context.WithTimeout(context.Background(), 1*time.Second)
 	go func(ctx context.Context) {
 		fmt.Println("Doing something...")
 		<-signals
 		tasks <- struct{}{}
-	}(ctx)
+		defer func() { cancelServ() }()
+	}(ctxShutDown)
 	<-tasks
-	<-time.After(1 * time.Second)
+	<-ctx.Done()
 	fmt.Println("End of main...")
 }
