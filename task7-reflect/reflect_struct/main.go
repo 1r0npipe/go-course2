25abd1b540f6d00f97a07f817d2b30d28f7fb7c0
Eugene Makhinya <jesonsev@gmail.com>
2021-03-12 23:55:49 +0000

    change if slice

--- task7-reflect/reflect_struct/main.go
+++ task7-reflect/reflect_struct/main.go
@@ -8,9 +8,9 @@ import (
 
 type person struct {
 	name   string
-	skills []Skills
+	skills []Skill
 }
-type Skills struct {
+type Skill struct {
 	name string
 	exp  uint32
 }
@@ -18,19 +18,20 @@ type Skills struct {
 func main() {
 	pers := &person{
 		name: "john",
-		skills: []Skills{
+		skills: []Skill{
 			{"c++", 2},
 			{"python", 3},
+			{"golang", 5},
 		},
 	}
 	mapTest := make(map[string]interface{}, 1)
 	mapTest["python"] = 5
-	result, err := changeIntoStruct(pers, mapTest)
+	_, err := changeIntoStruct(pers, mapTest)
 	if err != nil {
-		log.Fatal("Can't change struct by map")
+		log.Fatal("Can't change struct by map: ", err)
 	}
 
-	fmt.Println(result)
+	//fmt.Println(result)
 }
 
 func changeIntoStruct(in interface{}, mapInit map[string]interface{}) (interface{}, error) {
@@ -48,20 +49,13 @@ func changeIntoStruct(in interface{}, mapInit map[string]interface{}) (interface
 		typeField := val.Type().Field(i)
 		if typeField.Type.Kind() == reflect.Slice {
 			log.Println("We have the slice to parse next")
-			sli, ok := val.Field(i).Interface().([]Skills)
-			if !ok {
-				return nil, fmt.Errorf("can't get slice of stuct Skills")
-			}
-			for j := 0; j < len(sli); i += 1 {
-				for valuesMap, keyMap := range mapInit {
-					if sli[j].name == valuesMap {
-						realMapValue, ok := keyMap.(uint32)
-						if !ok {
-							return nil, fmt.Errorf("Can't extract real value from map")
-						}
-						sli[j].exp = realMapValue
-					}
+			for j := 0; j < val.Field(i).Len(); j += 1 {
+				itemStruct := val.Field(i).Index(j)
+				if itemStruct.Kind() != reflect.Struct {
+					return nil, fmt.Errorf("something wrong with Skill struct definition")
 				}
+				changeIntoStruct(itemStruct.Interface(), mapInit)
+				continue
 			}
 		}
 		if typeField.Type.Kind() == reflect.Struct {
@@ -69,6 +63,7 @@ func changeIntoStruct(in interface{}, mapInit map[string]interface{}) (interface
 			changeIntoStruct(val.Field(i).Interface(), mapInit)
 			continue
 		}
+
 	}
 	return val.Interface(), nil
 }
