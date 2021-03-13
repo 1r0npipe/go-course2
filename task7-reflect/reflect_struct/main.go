34f1d567fb00b64ba2ba5985bc68be9273fb22bd
Eugene Makhinya <jesonsev@gmail.com>
2021-03-13 18:13:46 +0000

    change to compare

--- task7-reflect/reflect_struct/main.go
+++ task7-reflect/reflect_struct/main.go
@@ -10,28 +10,32 @@ type person struct {
 	name   string
 	skills []Skill
 }
+type person2 struct {
+	name string
+	age  int
+}
 type Skill struct {
 	name string
 	exp  uint32
 }
 
 func main() {
-	pers := &person{
-		name: "john",
-		skills: []Skill{
-			{"c++", 2},
-			{"python", 3},
-			{"golang", 5},
-		},
-	}
+	//pers := &person{
+	//	name: "john",
+	//	skills: []Skill{
+	//		{"c++", 2},
+	//		{"python", 3},
+	//		{"golang", 5},
+	//	},
+	//}
+	pers2 := &person2{"bob", 34}
 	mapTest := make(map[string]interface{}, 1)
 	mapTest["python"] = 5
-	_, err := changeIntoStruct(pers, mapTest)
+	result, err := changeIntoStruct(*pers2, mapTest)
 	if err != nil {
 		log.Fatal("Can't change struct by map: ", err)
 	}
-
-	//fmt.Println(result)
+	fmt.Println(result)
 }
 
 func changeIntoStruct(in interface{}, mapInit map[string]interface{}) (interface{}, error) {
@@ -42,28 +46,36 @@ func changeIntoStruct(in interface{}, mapInit map[string]interface{}) (interface
 	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
-	if val.Kind() != reflect.Struct {
-		return nil, fmt.Errorf("the interface is not structure")
-	}
-	for i := 0; i < val.NumField(); i += 1 {
-		typeField := val.Type().Field(i)
-		if typeField.Type.Kind() == reflect.Slice {
-			log.Println("We have the slice to parse next")
-			for j := 0; j < val.Field(i).Len(); j += 1 {
-				itemStruct := val.Field(i).Index(j)
-				if itemStruct.Kind() != reflect.Struct {
-					return nil, fmt.Errorf("something wrong with Skill struct definition")
+	if val.Kind() == reflect.Struct {
+		fmt.Println("checking struct...")
+		for i := 0; i < val.NumField(); i += 1 {
+			typeField := val.Type().Field(i)
+			if typeField.Type.Kind() == reflect.Slice {
+				fmt.Println("We have the slice to parse next")
+				for j := 0; j < val.Field(i).Len(); j += 1 {
+					itemStruct := val.Field(i).Index(j)
+					fmt.Println("wow", itemStruct)
+					changeIntoStruct(itemStruct.Addr().Interface(), mapInit)
+					continue
 				}
-				changeIntoStruct(itemStruct.Interface(), mapInit)
+			}
+			if typeField.Type.Kind() == reflect.Struct {
+				fmt.Println("We have just one more struct inside")
+				changeIntoStruct(val.Field(i).Interface(), mapInit)
 				continue
 			}
+			for key, value := range mapInit {
+				if val.Field(i).Kind() == reflect.String {
+					f := val.Field(i)
+					if f == key {
+						if val.Field(i + 1).IsValid() {
+							val.Field(i + 1).Set(value)
+						}
+					}
+				}
+			}
 		}
-		if typeField.Type.Kind() == reflect.Struct {
-			log.Println("We have just one more struct inside")
-			changeIntoStruct(val.Field(i).Interface(), mapInit)
-			continue
-		}
-
 	}
+
 	return val.Interface(), nil
 }
