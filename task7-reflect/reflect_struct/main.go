a85f9de32b9dddef80ac2f1407d5f8551788cc20
Eugene Makhinya <jesonsev@gmail.com>
2021-03-13 21:36:19 +0000

    working

--- task7-reflect/reflect_struct/main.go
+++ task7-reflect/reflect_struct/main.go
@@ -16,7 +16,7 @@ type Skill struct {
 }
 
 func main() {
-	pers := person{
+	pers := &person{
 		FirstName: "john",
 		Skills: []Skill{
 			{"c++", int64(2)},
@@ -46,34 +46,28 @@ func changeIntoStruct(in interface{}, mapInit map[string]interface{}) (interface
 	if val.Kind() == reflect.Struct {
 		for i := 0; i < val.NumField(); i += 1 {
 			typeField := val.Type().Field(i)
-			fmt.Println(typeField.Name)
 			if typeField.Type.Kind() == reflect.Slice {
 				//fmt.Println("We have the slice to parse next")
 				for j := 0; j < val.Field(i).Len(); j += 1 {
 					itemStruct := val.Field(i).Index(j)
-					changeIntoStruct(itemStruct.Interface(), mapInit)
+					changeIntoStruct(itemStruct.Addr().Interface(), mapInit)
 					continue
 				}
 			}
 			if typeField.Type.Kind() == reflect.Struct {
-				//fmt.Println("We have just one more struct inside")
-				changeIntoStruct(val.Field(i).Interface(), mapInit)
+				fmt.Printf("We have just one more struct inside\n")
+				changeIntoStruct(val.Field(i).Addr().Interface(), mapInit)
 				continue
 			}
 			mapValue, ok := mapInit[typeField.Name]
 			if !ok {
 				continue
 			}
-			fmt.Printf("%+v - %+v\n", mapValue, typeField.Name)
-			switch val.Field(i).Type().Kind() {
-			case reflect.Int:
+			switch typeField.Type.Kind() {
+			case reflect.Int64:
 				val.Field(i).SetInt(mapValue.(int64))
-			case reflect.Float64:
-				val.Field(i).SetFloat(mapValue.(float64))
 			case reflect.String:
 				val.Field(i).SetString(mapValue.(string))
-			case reflect.Bool:
-				val.Field(i).SetBool(mapValue.(bool))
 			default:
 				return nil, fmt.Errorf("type mistamtch to assign the value")
 			}
