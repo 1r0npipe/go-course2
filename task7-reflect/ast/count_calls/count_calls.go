a13c3999cfb1243468658e782298c21dabba0e7b
Eugene Makhinya <jesonsev@gmail.com>
2021-03-14 18:46:00 +0000

    adding more tests

--- task7-reflect/ast/count_calls/count_calls.go
+++ task7-reflect/ast/count_calls/count_calls.go
@@ -1,5 +1,19 @@
 package countcalls
 
-func countAsyncCalls() {
+import (
+	"fmt"
+	"go/parser"
+	"go/token"
+)
 
+// CountAsyncCalls count all async call (goroutines) in fileName for funcName
+// function inside of fileName, returns numbers of calls or error
+func CountAsyncCalls(fileName, funcName string) (int, error) {
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, fileName, nil, 0)
+	if err != nil {
+		return 0, fmt.Errorf("Can't read file")
+	}
+
+	return countAsycCalls, nil
 }
--- task7-reflect/ast/main.go
+++ task7-reflect/ast/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/1r0npipe/go-course2/task7-reflect/ast/count_calls"
+import (
+	"fmt"
+	"github.com/1r0npipe/go-course2/task7-reflect/ast/count_calls"
+	"log"
+)
 
 func main() {
-	countcalls.cou
+	filename := "test.go"
+	funcName := "testFunc"
+	num, err := countcalls.CountAsyncCalls(filename, funcName)
+	if err != nil {
+		log.Fatal("Cant count the number of go func due to %+v\n", err)
+	}
+	fmt.Println("Number of go calls in file: %s at function: %s is %d\n",
+		filename,
+		funcName,
+		num)
 }
